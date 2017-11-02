7c0319a3e14fb34324abc1f9e8d663ebf9ef0267
tszwaiyan <tszwaiyan@cpp.edu>
2017-11-02 14:30:44 -0700

    added NoSelectionModel

---
+++ src/MiniTwitter/NoSelectionModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MiniTwitter;
+
+/**
+ *
+ * @author tszwaiyan
+ */
+class NoSelectionModel extends javax.swing.DefaultListSelectionModel{
+
+   @Override
+   public void setAnchorSelectionIndex(final int anchorIndex) {}
+
+   @Override
+   public void setLeadAnchorNotificationEnabled(final boolean flag) {}
+
+   @Override
+   public void setLeadSelectionIndex(final int leadIndex) {}
+
+   @Override
+   public void setSelectionInterval(final int index0, final int index1) { }
+    
+}
